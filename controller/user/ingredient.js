74cdd1e6f32d026f2addea85e96ae139d857ccac
alihmaidi <alihmaidi095@gmail.com>
2023-06-17 05:42:18 -0400

    edit

--- controller/user/ingredient.js
+++ controller/user/ingredient.js
@@ -3,7 +3,8 @@ const db = require("../../models");
 exports.getall = async (req, res, next) => {
 
     let category_id = req.query.category;
-    let nutritionals = JSON.parse(req.query.nutritionals);
+    let nutritionals = req.query.nutritionals;
+    nutritionals = (nutritionals != undefined) ? JSON.parse(nutritionals) : null;
     let ingredients = await db.ingredient.scope({ method: ["category", category_id] }).findAll({ include: [{ model: db.nutritional, through: { attributes: ["value"] } }, { model: db.unit }, { model: db.category1 }] });
     return res.success(ingredients, "this is all ingredients")
 
